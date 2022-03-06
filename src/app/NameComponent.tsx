aeb1a681a7e0d2e5958d1cdcd28f8ac5d7c5ae89
Kholev Alexandr <khalex8993@gmail.com>
2022-03-06 08:58:45 +0300

    add new

---
+++ src/app/NameComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NameComponent = () => (
+  <div>
+    <p className="firstName">Kholev</p>
+    <p className="secondName">Alexandr</p>
+    <p className="theeName">Urievich</p>
+  </div>
+);
+
+export default NameComponent;
