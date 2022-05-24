3518f8d2441949b6c2cd2c99336131a374f8aefc
Kholev Alexandr <khalex8993@gmail.com>
2022-05-24 19:29:26 +0300

    dropdounmenu

--- src/components/common/DropDownMenu/index.tsx
+++ src/components/common/DropDownMenu/index.tsx
@@ -10,11 +10,11 @@ const DropDownMenu = () => {
   const [isAdmin, setIsAdmin] = useState(true);
 
   const handler = () => {
-    setDropDownMenu(!dropDownMenu);
+    setTimeout(setDropDownMenu(!dropDownMenu:boolean), 1000);
   };
 
   return (
-    <div className={style.dropDownMenuContainer} onMouseEnter={handler}>
+    <div className={style.dropDownMenuContainer} onMouseEnter={handler} onMouseLeave={handler}>
       <div className={style.dropDownMenuBtn}>
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M20 21V19C20 17.9391 19.5786 16.9217 18.8284 16.1716C18.0783 15.4214 17.0609 15 16 15H8C6.93913 15 5.92172 15.4214 5.17157 16.1716C4.42143 16.9217 4 17.9391 4 19V21" stroke="#2a2f3778" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
